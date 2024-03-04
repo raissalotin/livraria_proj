2610ce6bb7fd8e50e5ebc26b90421f3a0f9c78b0
raissalotin <rahlotin@gmail.com>
2024-03-04 11:50:07 -0300

    criacao da model categoria

--- core/admin.py
+++ core/admin.py
@@ -8,6 +8,7 @@ from django.utils.translation import gettext_lazy as _
 from core import models
 
 
+
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
 
@@ -49,3 +50,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Categoria)
