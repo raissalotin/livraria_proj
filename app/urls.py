b66b871651823b06571de79a7abb1929e39fb60e
raissalotin <rahlotin@gmail.com>
2024-03-11 11:55:56 -0300

    criação da API para editora

--- app/urls.py
+++ app/urls.py
@@ -10,10 +10,11 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from core.views import UserViewSet
-from core.views import CategoriaViewSet
+from core.views import CategoriaViewSet, EditoraViewSet
 
 router = DefaultRouter()
 router.register(r"categorias", CategoriaViewSet)
+router.register(r"editoras", EditoraViewSet)
 router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
