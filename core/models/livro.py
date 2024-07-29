ee75c01748d882b5e69589d1f94e89f65114e228
raissalotin <rahlotin@gmail.com>
2024-07-29 11:32:22 -0300

    alteracao da model LIvro

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,9 +1,9 @@
 from django.db import models
-
 class Livro(models.Model):
-    titulo = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    quantidade = models.IntegerField(default=0,  null=True, blank=True)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
 
     def __str__(self):
-        return self.titulo
+        return f"({self.id}) {self.titulo} ({self.quantidade})"
