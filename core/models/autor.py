9668576cef20af3f426be4fcad4ec95d47f49853
raissalotin <rahlotin@gmail.com>
2024-07-29 11:49:46 -0300

    associacao categoria e editora à livro

--- core/models/autor.py
+++ core/models/autor.py
@@ -3,6 +3,11 @@ from django.db import models
 class Autor(models.Model):
     nome = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autores"
+
+
 
     def __str__(self):
         return self.nome
