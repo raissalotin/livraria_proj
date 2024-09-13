c44787733a0b74ba421c787b3c701bc14f3f0f87
raissalotin <rahlotin@gmail.com>
2024-09-13 14:40:15 -0300

    customização do admin

--- core/admin.py
+++ core/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core import models
+from core.models import Autor, Categoria, Editora, Livro, User
 
 
 
@@ -13,10 +13,10 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
 
     ordering = ["id"]
-    list_display = ["email", "name"]
+    list_display = ["email", "nome"]
     fieldsets = (
         (None, {"fields": ("email", "password")}),
-        (_("Personal Info"), {"fields": ("name",)}),
+        (_("Personal Info"), {"fields": ("nome",)}),
         (
             _("Permissions"),
             {
@@ -48,9 +48,36 @@ class UserAdmin(BaseUserAdmin):
         ),
     )
 
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    @admin.register(Autor)
+    class AutorAdmin(admin.ModelAdmin):
+        list_display = ('nome', 'email')
+    search_fields = ('nome', 'email')
+    list_filter = ('nome',)
+    ordering = ('nome', 'email')
+    list_per_page = 10
 
-admin.site.register(models.User, UserAdmin)
-admin.site.register(models.Categoria)
-admin.site.register(models.Editora)
-admin.site.register(models.Autor)
-admin.site.register(models.Livro)
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('descricao',)
+    search_fields = ('descricao',)
+    list_filter = ('descricao',)
+    ordering = ('descricao',)
+    list_per_page = 10
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ('nome','site')
+    search_fields = ('nome', 'site')
+    list_filter = ('nome','site')
+    ordering = ('nome', 'site')
+    list_per_page = 10
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'editora', 'categoria')
+    search_fields = ('titulo', 'editora__nome', 'categoria__descricao')
+    list_filter = ('editora', 'categoria')
+    ordering = ('titulo', 'editora', 'categoria')
+    list_per_page = 25
